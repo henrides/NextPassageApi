6c5d725058e2f9ee385cba45dfae9633c1d9af8a
Marc-Olivier Mercier <alt@bond>
2019-04-13 23:31:20 -0400

    Return empty passage list when stop is not found.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,31 +6,26 @@ admin.initializeApp();
 
 const app = express();
 
-app.route('/nextPassages').post((request, response) => {
+app.route('/nextPassages').post(async (request, response) => {
     console.log(JSON.stringify(request.body));
     if (!request || !request.body || !request.body.trips) {
         response.status(400).send('No trips');
         return;
     }
 
-    const passagesP: any = [];
-    request.body.trips.forEach((trip: any) => {
-        passagesP.push(admin.database().ref('/tripUpdates/' + trip.route + '/' + trip.stop).once('value'));
-    });
+    const responseTrips: Array<any> = [];
+    for (const trip of request.body.trips) {
+        try {
+            const passages: any = await admin.database().ref('/tripUpdates/' + trip.route + '/' + trip.stop).once('value');
+            const passagesArr: any = passages && passages.val() ? passages.val() : [];
+            responseTrips.push({route: trip.route, stop: trip.stop, passages: passagesArr});
+        } catch(error) {
+            responseTrips.push({route: trip.route, stop: trip.stop, passages: []});
+            console.log(JSON.stringify(error));
+        }
+    }
 
-    Promise.all(passagesP).then((values) => {
-        response.send(values.map((x: any) => {
-            console.log(JSON.stringify(x.val()));
-            return {
-                route: x.val().routeId,
-                stop: x.val().stopId,
-                passages: x.val().nextPassages
-            };
-        }));
-    }).catch((error: any) => {
-        console.log(JSON.stringify(error));
-        response.status(400).send('Invalid trip');
-    });
+    response.send(responseTrips);
 });
 
 exports.api = functions.https.onRequest(app);
